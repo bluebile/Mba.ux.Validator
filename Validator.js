7e267c766928cda842d411996b8877352df9d98b
ramonornela <ramon.ornela@gmail.com>
2015-06-24 16:01:36 -0300

    [UPDATE]
    - Adicao estrutura

---
+++ Validator.js
@@ -0,0 +1,24 @@
+Ext.define('Mba.util.Validator', {
+
+    requires: ['Ext.data.Validations'],
+
+    singleton: true,
+
+    constructor: function() {
+        Ext.merge(this, Ext.data.Validations, {
+            maxValue: function(config, value) {
+                if (value === undefined || value === null) {
+                    return false;
+                }
+
+                var max = config.max;
+
+                if (value > max) {
+                    return false;
+                }
+
+                return true;
+            }
+        });
+    }
+});
